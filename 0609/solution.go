31f0f922c7fde220102dfb2db10ed45f786e9329
Lhz <869695227@qq.com>
2020-06-09 19:23:18 +0800

    0609

---
+++ 0609/solution.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	output := translateNum(122589)
+
+	fmt.Println(output)
+
+}
+
+func translateNum(num int) int {
+	src := strconv.Itoa(num)
+    p, q, r := 0, 0, 1
+    for i := 0; i < len(src); i++ {
+        p, q, r = q, r, 0
+        r += q
+        if i == 0 {
+            continue
+        }
+        pre := src[i-1:i+1]
+        if pre <= "25" && pre >= "10" {
+            r += p
+        }
+    }
+    return r
+}
