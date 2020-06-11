a2e522a73b8a0b270f859c3df9b7d2cba2e78cff
Lhz <869695227@qq.com>
2020-06-11 19:38:50 +0800

    0611-submit

--- 0611/solution.go
+++ 0611/solution.go
@@ -1,1 +1,52 @@
+package main
 
+import "fmt"
+
+func main() {
+	T := []int{73, 74, 75, 71, 69, 72, 76, 73}
+	fmt.Println(dailyTemperatures1(T))
+}
+
+func dailyTemperatures(T []int) []int {
+	l := len(T)
+	if l == 1 {
+		return []int{0}
+	}
+	output := make([]int, l)
+
+	for i := 0; i < l-1; i++ {
+		if T[i] < T[i+1] {
+			output[i] = 1
+		} else {
+			for j := i + 1; j < l; j++ {
+				if T[j] > T[i] {
+					output[i] = j - i
+					break
+				}
+			}
+		}
+	}
+	return output
+}
+
+func dailyTemperatures1(T []int) []int {
+	l := len(T)
+	if l == 1 {
+		return []int{0}
+	}
+	output := make([]int, l)
+
+	for i := 0; i < l-1; i++ {
+		if T[i] < T[i+1] {
+			output[i] = 1
+		} else {
+			for j := i + 1; j < l; j++ {
+				if T[j] > T[i] {
+					output[i] = j - i
+					break
+				}
+			}
+		}
+	}
+	return output
+}
