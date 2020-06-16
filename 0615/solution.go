fdc60d8b91669ff05cc6b523e2b0e709610a5551
Lhz <869695227@qq.com>
2020-06-16 09:04:08 +0800

    0616

--- 0615/solution.go
+++ 0615/solution.go
@@ -1,1 +1,42 @@
+package main
 
+import "fmt"
+
+func main() {
+
+	input := []string{"flower", "flow", "flight"}
+	fmt.Printf("output: %s ", longestCommonPrefix(input))
+}
+
+func longestCommonPrefix(strs []string) string {
+	output := ""
+	minSize := -1
+	for _, str := range strs {
+
+		fmt.Println(str)
+		iSize := len(str)
+		fmt.Println(iSize)
+		if minSize == -1 || iSize < minSize {
+			minSize = iSize
+		}
+	}
+
+	fmt.Printf("minSize: %d\n", minSize)
+
+	for i := 0; i < minSize; i++ {
+		temp := strs[0][i : i+1]
+		flag := true
+		for _, str := range strs {
+			if str[i:i+1] != temp {
+				flag = false
+				break
+			}
+		}
+		if flag {
+			output = output + temp
+		} else {
+			break
+		}
+	}
+	return output
+}
