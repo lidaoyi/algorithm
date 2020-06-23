8144725cee91ad1e0b6ef992c1f85730e046bda2
Lhz <869695227@qq.com>
2020-06-23 09:20:48 +0800

    0623

--- 0622/solution.go
+++ 0622/solution.go
@@ -1,9 +1,44 @@
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+func main() {
+	pattern := "aaaa"
+	value := "dogcatdogcatdogcatdogcat"
+	fmt.Println(patternMatching(pattern, value))
 }
 
 func patternMatching(pattern string, value string) bool {
+	is, count := isSinglePattern(pattern)
+	if is {
+		size := len(value)
+		subSize := size / count
+		temp := value[0 : subSize-1]
+		for i := 1; i < count; i++ {
+			if temp != value[i*subSize:(i+1)*subSize-1] {
+				return false
+			}
+		}
+		return true
+	} else {
+
+	}
 	return false
+}
+
+// isSinglePattern 是否 aaa 模式
+// return 是/否 a的个数
+func isSinglePattern(pattern string) (bool, int) {
+	// temp:=
+	temp := pattern[0:1]
+	count := 0
+	for _, c := range pattern {
+		if string(c) != temp {
+			return false, 0
+		}
+		count++
+	}
+	return true, count
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module algorithm
 
 go 1.13
 
-require golang.org/x/tools v0.0.0-20200610052024-8d7dbee4c8ae // indirect
+require (
+	golang.org/x/tools v0.0.0-20200610052024-8d7dbee4c8ae // indirect
+	honnef.co/go/tools v0.0.1-2020.1.4
+)
